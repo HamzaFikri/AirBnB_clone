7754e5c4afdc84a34b326cc149cccf2441efe3d2
HamzaFikri <mohammed.hamza.fikri@gmail.comgit config --global user.name HamzaFikrigit config --global user.email mohammed.hamza.fikri@gmail.com>
2023-08-11 16:50:21 -0700

    Refactored functional descriptions

---
+++ models/user.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Here you can find the class User
+"""
+
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Here start the class user"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes user"""
+        super().__init__(*args, **kwargs)
